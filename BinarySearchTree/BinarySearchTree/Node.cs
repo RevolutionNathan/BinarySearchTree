2f4e5731ebf3197f9a059a4f11df3ac140b80406
Nathan Whitcomb <nathanwhitcomb@MacBook-Pro.local>
2017-05-15 14:15:58 -0500

    initial commit

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int nodeValue;
+        public int left;
+        public int right;
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/TreeConstructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class TreeConstructor
+    {
+
+        Node root = new Node(0, 0, 0);
+
+
+    }
+
+
+}
